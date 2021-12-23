02d6f3d089696b305ad3bda307c2f0d17e6c9c03
EidAbdullahi <https://github.com/EidAbdullahi>
2021-12-23 07:26:43 -0500

    commit changes

--- shipment/settings.py
+++ shipment/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # MEDIA_URL = '/media/'
 # # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
